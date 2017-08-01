2331bb322a16c878db43a80787c4e36fe5c13bbf
Alex Milbert <alex.milbert@gmail.com>
2017-08-01 09:28:22 -0700

    Coder init

--- js_lib/coder.js
+++ js_lib/coder.js
@@ -39,4 +39,49 @@ class Coder extends MovingObject {
       this.vel[0] += 2;
     }
   }
+
+  checkBumped() {
+    if (this.bumped) {
+      this.jumping = false;
+      this.vel[0] = 0;
+      if (this.pos[1] <= 280) {
+        this.vel[1] += 15;
+      } else if (this.pos[1] >= 450) {
+        this.vel[1] = 0;
+        this.pos[1] = 450;
+      }
+    }
+  }
+
+  jump() {
+    if (this.pos[1] === 450 && this.jumping === false && this.bumped === false) {
+      this.jumpAudio.play();
+      this.vel[1] = -15;
+      this.jumping = true;
+    } else if (this.pos[1] <= 280) {
+      this.vel[1] += 15;
+    } else if (this.pos[1] >= 450) {
+      this.vel[1] = 0;
+      this.pos[1] = 450;
+    }
+  }
+
+  noJump() {
+    if (this.jumping === true) {
+      this.vel[1] = 15;
+      this.jumping = false;
+    } else if (this.pos[1] >= 450) {
+      this.vel[1] = 0;
+      this.pos[1] = 450;
+      this.jumping = false;
+    }
+  }
+
+  relocate() {
+    this.pos = [450, 400];
+    this.vel = [0,0];
+  }
 }
+
+Coder.RADIUS = 65;
+module.exports = Coder;
